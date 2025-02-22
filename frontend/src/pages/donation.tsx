f042fb4e58cdef09cd7833627e204c5de0e952c4
Aadish Jain <aadishjain017@gmail.com>
2025-02-22 23:50:01 +0530

    TS fix

--- frontend/src/pages/donation.tsx
+++ frontend/src/pages/donation.tsx
@@ -1,7 +1,6 @@
 // import React from 'react'
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 
 function Donation() {
   return (
